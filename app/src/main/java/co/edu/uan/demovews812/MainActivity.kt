5ba79406f343b6dea5b7c2f3e6e425feeadd9d65
wjforero <1499698+wjforero@users.noreply.github.com>
2025-08-21 20:37:43 -0500

    mostrar el puntaje del usuario

--- app/src/main/java/co/edu/uan/demovews812/MainActivity.kt
+++ app/src/main/java/co/edu/uan/demovews812/MainActivity.kt
@@ -2,6 +2,7 @@ package co.edu.uan.demovews812
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,12 +11,15 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
-    var numLeft = (0..100).random()
-    var numRight = (0..100).random()
+    var numLeft = 0
+    var numRight = 0
+    var score = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        initNumbers()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -27,22 +31,34 @@ class MainActivity : AppCompatActivity() {
         val btnRight = findViewById<Button>(R.id.btnRight)
 
         btnLeft.setOnClickListener {
-            if(numLeft >= numRight)
-                Toast.makeText(this, "Ganaste :)", Toast.LENGTH_LONG).show()
-            else
-                Toast.makeText(this, "Perdiste :(", Toast.LENGTH_LONG).show()
-            numLeft = (0..100).random()
-            numRight = (0..100).random()
+            mostrarMensajes(numLeft, numRight)
+            initNumbers()
         }
 
         btnRight.setOnClickListener {
-            if(numLeft <= numRight)
-                Toast.makeText(this, "Ganaste :)", Toast.LENGTH_LONG).show()
-            else
-                Toast.makeText(this, "Perdiste :(", Toast.LENGTH_LONG).show()
-            numLeft = (0..100).random()
-            numRight = (0..100).random()
+            mostrarMensajes(numRight, numLeft)
+            initNumbers()
         }
 
     }
+
+    fun initNumbers() {
+        numLeft = (0..100).random()
+        numRight = (0..100).random()
+    }
+
+    fun mostrarMensajes(greater: Int, less:Int) {
+        if(greater >= less) {
+            Toast.makeText(this, "Ganaste :)", Toast.LENGTH_LONG).show()
+            score += 3
+        }
+        else {
+            Toast.makeText(this, "Perdiste :(", Toast.LENGTH_LONG).show()
+            score -= 3
+        }
+        val textScore = findViewById<TextView>(R.id.txtScore)
+        textScore.text = "Score: $score"
+
+    }
+
 }
